1cb80da568f126b138f10d6f3a1552d88a414696
jacob stokes-warner <jacobstokeswarner@gmail.com>
2021-02-04 12:25:05 -0600

    added the problem to my code as comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+
+C# Console app assignment:
+Create a console app menu with options for running the programs below. Developers make sure your menu repeats until the user chooses to quit.
+You will also need to CREATE A MENU CLASS WITH A METHOD FOR EACH PROGRAM BELOW. INCLUDING A METHOD FOR PRINTING THE MENU.
+
+A: Input/Output: Write a program to prompt the user for their name, and greet them by name.
+B: Numeric Types: Write a program to calculate the area of a rectangle and print the answer to the console. You should prompt the user for the demensions.
+(what data types should the dimensions be?)
+C: Numeric types: Write a program that asks a user for the number of miles they have driven and the amount of gas they have consumed(in gallons) and print
+their miles per gallon.
+D: Strings: The first sentence of Alice's adventures in wonderland is below. Store this sentence in a string, and then prompt the user for a string search
+for within this string. Print whether or not the search term was found. See if you can make the search case-insensitive, so that the search for "alice" 
+prints true.
+
+Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her 
+sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'
+*/
+using System;
 
 namespace dotnet_practice_project
 {
